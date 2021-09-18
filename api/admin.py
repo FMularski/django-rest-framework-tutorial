5934d06d4db03e763a5379357ab453f113d299fe
Filip Mularski <mularskif@gmail.com>
2021-09-18 16:46:02 +0200

    created Task model

--- api/admin.py
+++ api/admin.py
@@ -3,4 +3,9 @@ from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
+class TaskAdmin(admin.ModelAdmin):
+    list_display = 'title', 'completed'
+
+
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Task, TaskAdmin)
--- api/models.py
+++ api/models.py
@@ -5,3 +5,14 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = 'title',
+
+    def __str__(self):
+        return self.title
+
