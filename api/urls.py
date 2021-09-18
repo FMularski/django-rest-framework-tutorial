409c04f17e242d9438e357e9e143b56629d4fe8c
Filip Mularski <mularskif@gmail.com>
2021-09-18 17:39:10 +0200

    update and delete endpoints

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.api_overview, name='api_overview'),
     path('task-list/', views.task_list, name='task_list'),
     path('task-detail/<int:pk>/', views.task_detail, name='task_detail'),
-    path('task-create/', views.task_create, name='task_create')
+    path('task-create/', views.task_create, name='task_create'),
+    path('task-update/<int:pk>/', views.task_update, name='task_update'),
+    path('task-delete/<int:pk>/', views.task_delete, name='task_delete')
 ]
--- api/views.py
+++ api/views.py
@@ -44,5 +44,25 @@ def task_create(request):
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)
-        
+
     return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+def task_update(request, pk):
+    task = Task.objects.get(pk=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    return Response(serializer.erros, status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def task_delete(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.delete()
+
+    return Response(status=status.HTTP_204_NO_CONTENT)
