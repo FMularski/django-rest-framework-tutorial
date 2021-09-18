8e0cda512031273b349bb5efc3e157eaec503bc6
Filip Mularski <mularskif@gmail.com>
2021-09-18 17:24:05 +0200

    get detail and create new endpoints

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.api_overview, name='api_overview'),
-    path('task-list/', views.tasks, name='task_list'),
+    path('task-list/', views.task_list, name='task_list'),
+    path('task-detail/<int:pk>/', views.task_detail, name='task_detail'),
+    path('task-create/', views.task_create, name='task_create')
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import TaskSerializer
@@ -21,8 +22,27 @@ def api_overview(request):
 
 
 @api_view(['GET'])
-def tasks(request):
+def task_list(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
 
-    return Response(serializer.data)
+    return Response(serializer.data, status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def task_detail(request, pk):
+    task = Task.objects.get(pk=pk)
+    serializer = TaskSerializer(task, many=False)
+
+    return Response(serializer.data, status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def task_create(request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+        
+    return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
